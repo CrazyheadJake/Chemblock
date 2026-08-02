5bf79e9e46bc6162db8713b6379e99306b124249
CrazyheadJake <jake.d.moleski@gmail.com>
2026-08-02 10:37:31 -0700

    Add custom space platform to start with and uptade to work on latest experimental: 2.1.12

--- control.lua
+++ control.lua
@@ -1,20 +1,6 @@
 local util = require("util")
 
-script.on_event(defines.events.on_player_changed_position,
-  function(event)
-    local player = game.get_player(event.player_index) -- get the player that moved
-    -- if they're currently controlling the character
-    if player.controller_type == defines.controllers.character then
-      -- and wearing our armor
-      if player.get_inventory(defines.inventory.character_armor).get_item_count("fire-armor") >= 1 then
-        -- create the fire where they're standing
-        player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"}
-      end
-    end
-  end
-)
-
-local starting_pack = "space-platform-starter-pack"
+local starting_pack = "space-station-starter-pack"
 local starting_planet = "nauvis"
 
 local function starting_items()
---
+++ data.lua
@@ -0,0 +1,28 @@
+local function make_tile_area(area, name)
+  local result = {}
+  local left_top = area[1]
+  local right_bottom = area[2]
+  for x = left_top[1], right_bottom[1] do
+    for y = left_top[2], right_bottom[2] do
+      table.insert(result, {position = {x, y}, tile = name})
+    end
+  end
+  return result
+end
+
+local spaceStationStartPack = table.deepcopy(data.raw["space-platform-starter-pack"]["space-platform-starter-pack"]) --[[@as SpacePlatformStarterPackPrototype]]
+
+spaceStationStartPack.name = "space-station-starter-pack"
+spaceStationStartPack.icons = {
+    {
+    icon = spaceStationStartPack.icon,
+    icon_size = spaceStationStartPack.icon_size,
+    tint = {r=1,g=0,b=1,a=0.3}
+  },
+}
+
+spaceStationStartPack.tiles = make_tile_area({{-20, -20}, {19, 19}}, "space-platform-foundation")
+spaceStationStartPack.initial_items = nil
+spaceStationStartPack.create_electric_network = false
+
+data:extend{spaceStationStartPack}
