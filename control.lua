e3fbb71a4c82567097df6e127e80e29d7a1efc2c
CrazyheadJake <jake.d.moleski@gmail.com>
2026-08-05 20:31:08 -0700

    Add smelting ores when they are dropped on the ground

--- control.lua
+++ control.lua
@@ -83,9 +83,47 @@ script.on_nth_tick(30, function()
   end
 end)
 
+local SMELTING_TIME = 10
+local SMELTABLE_ITEMS = {["iron-ore"] = "iron-plate", ["copper-ore"] = "copper-plate"}
+
+script.on_nth_tick(60, function(event)
+  print("checking")
+  local dropped_items = storage.dropped_items or {}
+  for index, data in ipairs(dropped_items) do
+    if event.tick >= data.end_tick then
+      local item = data.entity
+      if not item.valid then
+        table.remove(storage.dropped_items, index)
+        goto continue
+      end
+      local result = SMELTABLE_ITEMS[item.stack.name]
+      local surface = item.surface
+      local position = item.position
+      item.destroy()
+      surface.spill_item_stack({
+        position = position,
+        stack = {name=result, count=1}
+      })
+      table.remove(storage.dropped_items, index)
+      
+    end
+      ::continue::
+  end
+end)
+
+script.on_event(defines.events.on_player_dropped_item, function(event)
+  local item = event.entity
+  if SMELTABLE_ITEMS[item.stack.name] == nil then
+    return
+  end
+  storage.dropped_items = storage.dropped_items or {}
+  table.insert(storage.dropped_items, {entity = item, end_tick = event.tick + 60 * SMELTING_TIME})
+end)
+
 
 script.on_init(function()
   game.forces.player.unlock_space_platforms()
+  storage.dropped_items = {}
 
   remote.call("freeplay", "set_disable_crashsite", true)
   remote.call("freeplay", "set_skip_intro", true)
